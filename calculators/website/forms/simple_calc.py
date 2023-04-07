90f7dcbfa852709c51de4594a68da5bb5b09c6d1
Arepo <jinksy@gmail.com>
2023-04-07 18:21:59 +0100

    Add FlaskWTForm for simple calc fields

---
+++ calculators/website/forms/simple_calc.py
@@ -0,0 +1,46 @@
+import pdb
+from flask_wtf import FlaskForm
+from wtforms import DecimalField
+from wtforms.validators import InputRequired, NumberRange
+
+class SimpleCalcForm(FlaskForm):
+  extinction_given_survival = DecimalField('Extinction Given Survival', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  extinction_given_preindustrial = DecimalField('Extinction Given Preindustrial', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  extinction_given_industrial = DecimalField('Extinction Given Industrial', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+
+  extinction_given_perils = DecimalField('Extinction Given Perils', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  survival_given_perils = DecimalField('Survival Given Perils', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  preindustrial_given_perils = DecimalField('Preindustrial Given Perils', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  industrial_given_perils = DecimalField('Industrial Given Perils', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  multiplanetary_given_perils = DecimalField('Multiplanetary Given Perils', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  # interstellar_given_perils = DecimalField('Interstellar Given Perils', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+
+  extinction_given_multiplanetary = DecimalField('Extinction Given Multiplanetary', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  survival_given_multiplanetary = DecimalField('Survival Given Multiplanetary', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  preindustrial_given_multiplanetary = DecimalField('Preindustrial Given Multiplanetary', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  industrial_given_multiplanetary = DecimalField('Industrial Given Multiplanetary', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  perils_given_multiplanetary = DecimalField('Perils Given Multiplanetary', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+  # interstellar_given_multiplanetary = DecimalField('Interstellar Given Multiplanetary', validators=[InputRequired(), NumberRange(min=0, max=1)], default=0)
+
+  def validate(self, extra_validators=None):
+    initial_validation = super(SimpleCalcForm, self).validate(extra_validators)
+    # pdb.set_trace()
+    if not initial_validation:
+      return False
+
+    valid = True
+    if (self.extinction_given_perils.data + self.survival_given_perils.data
+        + self.preindustrial_given_perils.data + self.industrial_given_perils.data
+        + self.multiplanetary_given_perils.data > 1):
+      self.extinction_given_perils.errors.append('Perils transition probabilities must sum to <= 1')
+      valid = False
+
+    if (self.extinction_given_multiplanetary.data + self.survival_given_multiplanetary.data
+        + self.preindustrial_given_multiplanetary.data + self.industrial_given_multiplanetary.data
+        + self.perils_given_multiplanetary.data > 1):
+      self.extinction_given_multiplanetary.errors.append('Multiplanetary transition probabilities must sum to <= 1')
+      valid = False
+
+    return valid
+
+
---
+++ calculators/website/webapp.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import pdb
+
+from forms.simple_calc import SimpleCalcForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'seekrit' # TODO Any reason to care about this in the absence of a
+# database except that Flask/WTForms seem to require it?
+
+
+@app.route('/', methods=['GET', 'POST'])
+def simple_calc():
+  form = SimpleCalcForm()
+  success = ""
+  if form.validate_on_submit():
+    pdb.set_trace()
+    success = "Well done!"
+  return render_template('simple_calc.html', form=form, success=success)
+
+# @app.post("/")
+# def stuff():
+#   return "<p>g'bye, World!</p>"
