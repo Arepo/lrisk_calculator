26c9bed414f4242ffe98081634181f3696019f5f
Sasha Cooper <jinksy@gmail.com>
2023-10-07 16:13:25 +0200

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-setuptools.setup(name='transitional_probabilities',
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setuptools.setup(name='lrisks',
 version='0.1',
 description='Probability functions for the l-risk calculator',
 url='#',
 author='Arepo',
-install_requires=['opencv-python'],
+install_requires=requirements,
 author_email='',
 packages=setuptools.find_packages(),
 zip_safe=False)
