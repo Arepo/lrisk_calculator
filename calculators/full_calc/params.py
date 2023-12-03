6e17ef1309259166dd7c8df874980600e90e376f
Sasha Cooper <jinksy@gmail.com>
2023-12-03 15:54:54 +0100

    Add convenience methods to params.py

--- calculators/full_calc/params.py
+++ calculators/full_calc/params.py
@@ -23,3 +23,30 @@ class Params:
     def describe(self):
         """Returns a description of the parameters"""
         return yaml.dump(self.dictionary, default_flow_style=False)
+
+    def get_param_keys(self, nested_dict=None, parent_key='', key_separator='_'):
+        keys = []
+        if nested_dict == None:
+            nested_dict = self.dictionary
+        for key, value in nested_dict.items():
+            new_key = parent_key + key_separator + key if parent_key else key
+            if isinstance(value, dict):
+                # Recursively call the function if the value is another dictionary
+                keys.extend(self.get_param_keys(value, new_key, key_separator))
+            else:
+                # Append the key to the list if it's the lowest level
+                keys.append(new_key)
+        return keys
+
+    def get_param_values(self, nested_dict=None):
+        values = []
+        if nested_dict == None:
+            nested_dict = self.dictionary
+        for value in nested_dict.values():
+            if isinstance(value, dict):
+                # Recursively call the function if the value is another dictionary
+                values.extend(self.get_param_values(value))
+            else:
+                # Append the value to the list if it's the lowest level
+                values.append(value)
+        return values
