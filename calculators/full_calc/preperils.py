7889cf965a8af4f61365faed2e30a273db821dc3
Sasha Cooper <jinksy@gmail.com>
2024-02-27 16:42:32 +0800

    Tweak preperils arithmetic

--- calculators/full_calc/preperils.py
+++ calculators/full_calc/preperils.py
@@ -18,8 +18,9 @@ def extinction_given_preindustrial(k):
 
     expected_time_in_years = base_expected_time_in_years * stretch_per_reboot ** k
 
-    return 1 - ((1 - p_params.base_annual_extinction_probability) ** expected_time_in_years)
+    probability_increase = p_params.per_reboot_annual_extinction_probability_multiplier ** k
 
+    return 1 - ((1 - p_params.base_annual_extinction_probability * probability_increase) ** expected_time_in_years)
 
 def industrial_given_preindustrial(k, k1):
     """Calculate probability of extinction from preindustrial state in the kth civilisation,
@@ -28,7 +29,6 @@ def industrial_given_preindustrial(k, k1):
         # We can't transition to different civilisations from a preperils state
         return 0
     return 1 - extinction_given_preindustrial(k)
-
 
 def extinction_given_industrial(k):
     """Calculate probability of extinction from an industrial state in the kth civilisation,
@@ -45,7 +45,10 @@ def extinction_given_industrial(k):
 
     expected_time_in_years = base_expected_time_in_years * stretch_per_reboot ** k
 
+    probability_increase = i_params.per_reboot_annual_extinction_probability_multiplier ** k
+
     return (1 - (1 - i_params.base_annual_extinction_probability
+                     * probability_increase
                      * i_params.annual_extinction_probability_coefficient)
                 ** expected_time_in_years)
 
