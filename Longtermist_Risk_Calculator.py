ee6cb7d995cc1334d9da71f969b7e7d0e597c29c
Sasha Cooper <jinksy@gmail.com>
2024-03-02 14:35:29 +0800

    Tweak text on Streamlit page

--- Longtermist_Risk_Calculator.py
+++ Longtermist_Risk_Calculator.py
@@ -132,7 +132,7 @@ def make_on_change_callback(transition_name, origin_state):
     return callback
 
 
-st.write("#### The tool below allows you to estimate the transitional probabilities represented in the following diagram (descriptions below):")
+st.write("#### The tool below allows you to give credences representing the transitional probabilities in the following diagram (descriptions below):")
 
 st.image('https://39669.cdn.cke-cs.com/cgyAlfpLFBBiEjoXacnz/images/a4fab3864e51d5ecf59ea5a9d82e9ed8cbb98e30d914ba37.png/w_1170')
 
