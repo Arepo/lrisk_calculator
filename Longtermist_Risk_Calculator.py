95fc7c8cf179524c6c2f99fb13cc78d6b6b45d79
Sasha Cooper <jinksy@gmail.com>
2024-03-02 13:49:10 +0800

    Update simple calc diagram

--- Longtermist_Risk_Calculator.py
+++ Longtermist_Risk_Calculator.py
@@ -134,7 +134,7 @@ def make_on_change_callback(transition_name, origin_state):
 
 st.write("#### The tool below allows you to estimate the transitional probabilities represented in the following diagram (descriptions below):")
 
-st.image('https://res.cloudinary.com/cea/image/upload/f_auto,q_auto/v1/mirroredImages/ouuj93CPymfnvu8uQ/ytdctispib3doi9emjhb')
+st.image('https://39669.cdn.cke-cs.com/cgyAlfpLFBBiEjoXacnz/images/a4fab3864e51d5ecf59ea5a9d82e9ed8cbb98e30d914ba37.png/w_1170')
 
 st.write("""_Extinction_: Extinction of whatever type of life you value any time inclusively between now and our sun’s death (i.e. any case where we've failed to develop interplanetary technology that lets us escape the event).
 
